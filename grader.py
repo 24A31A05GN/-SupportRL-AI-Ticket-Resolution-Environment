ab376997a6aebe3df06658477658455bfb3306e9
24A31A05GN <revathich624@gmail.com>
2026-04-11 12:10:57 +0530

    Update grader.py

--- grader.py
+++ grader.py
@@ -1,6 +1,6 @@
 def grade(task, action):
     correct = task.get("correct_action", "")
     if action.lower().strip() == correct.lower().strip():
-        return 0.8
+        return 0.75
     else:
-        return 0.2
+        return 0.25
